64e38c835885ac6ddce64883c95d7f10d389173b
papa-docs <papa-docs1@proton.me>
2025-07-13 23:12:29 +0300

    Update values.js

--- FAKE/values.js
+++ FAKE/values.js
@@ -1,29 +1,29 @@
 // values.js
 
 // Основные данные 
-var fio               = "Таценко Валерiй Петров";
-var fio_en            = "Tatsenko Valerii";
-var birth             = "08.10.1978"; //дата рождения
-var date_give         = "03.11.2022"; //Дата видачи
-var date_out          = "03.11.2032"; // действителен до
-var organ             = "1801"; //орган что выдал документ
+var fio               = "Iванченко Роман Iванович";
+var fio_en            = "Ivanchenko Roma";
+var birth             = "20.10.1985"; //дата рождения
+var date_give         = "28.08.2020"; //Дата видачи
+var date_out          = "28.08.2030"; // действителен до
+var organ             = "6351"; //орган что выдал документ
 var rnokpp            = "2877009110"; //ИНН
-var uznr              = "19781008-04493"; //Номер записи
+var uznr              = "19851020-09616"; //Номер записи
 var pass_number       = "008420840"; //номер паспорта
 
 var registeredOn      = "03.11.2023"; //дата регистрации
 
 // Прописка
-var legalAdress       = "М. Житомир Вул. Космонавтів, Будинок 6 кв. 16"; //Место проживание
-var live              = "Житомир, Житомирська область"; //Место рождение 
-var bank_adress       = "М. Житомир Вул. Космонавтів, Будинок 6 кв. 16"; //Место жительства указано в банке
+var legalAdress       = "М. Красний Лиман"; //Место проживание
+var live              = "Красний Лиман, Донецкой область"; //Место рождение 
+var bank_adress       = "М. Красний Лиман"; //Место жительства указано в банке
 
 var sex               = "Ч";
 var sex_en            = "M";
 
 // Данные для Прав
 var rights_categories = "B"; //Категории
-var prava_number      = "AUX729537"; // номер прав
+var prava_number      = "AUX723727"; // номер прав
 var prava_date_give   = "04.09.2016"; //Дата выдачи Прав
 var prava_date_out    = "04.09.2036"; //Действителен ДО
 var pravaOrgan        = "1552"; //орган который выдал
@@ -41,7 +41,7 @@ var form              = "Очная";
 
 
 // заграник
-var zagran_number     = "GE696574"; //номер загран
+var zagran_number     = "FZ317661"; //номер загран
 var dateGiveZ         = "21.12.2022"; //выдан загран
 var dateOutZ          = "21.12.2032"; //коньчаеться загран
 
@@ -60,10 +60,10 @@ var isStudyEnabled    = false;
 
 // Пути к нужным фото    ?timestamp=N   "photo.jpg?timestamp=N" динамически: var url = "photo.jpg?timestamp=" + N;
 
-var photo_passport = "1.png"; //эДок, паспорт,
-var photo_rights   = "zagran2.png"; //права
-var photo_students = "zagran2.png"; //диплом, студ
-var photo_zagran   = "zagran.png"; //загран
+var photo_passport = "2.png"; //эДок, паспорт,
+var photo_rights   = "права.png"; //права
+var photo_students = "загран.png"; //диплом, студ
+var photo_zagran   = "загран.png"; //загран
 
 var signPng           = "sign.png"; //подпись
 // Можно добавить еще, если надо
